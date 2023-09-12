08ae62e1e7402179bd446b28d05c2f6761ba4dd7
steven dulworth <crowscoupetech@proton.me>
2023-09-12 17:29:49 -0500

    filtering arrays

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+let drivers = [{name:'Bobby'}, {name:'Sammy'}, {name:'Sally'}, {name:'Annette'}, {name:'Sarah'}, {name:'bobby'}];
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.slice(0, letters.length) === letters);
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
